9e27061d4bb5a014a2dd81304b52960bddcd9f8f
Esca-MMC <esca.mmc@gmail.com>
2020-07-13 15:03:59 -0500

    Move Extensions into separate file

--- WaterproofItems/ModEntry.cs
+++ WaterproofItems/ModEntry.cs
@@ -47,25 +47,4 @@ namespace WaterproofItems
                 HarmonyPatch_FloatingItemVisualEffect.ApplyPatch();
         }
     }
-
-    /// <summary>A static class containing any extensions shared by separate parts of this mod.</summary>
-    public static class Extensions
-    {
-        /// <summary>Indicates whether a <see cref="Debris"/> instance contains or represents an item.</summary>
-        /// <param name="debris">The debris instance.</param>
-        /// <returns>True if the debris contains or represents an item; otherwise false.</returns>
-        public static bool IsAnItem(this Debris debris)
-        {
-            if
-            (
-                debris.debrisType == Debris.DebrisType.OBJECT
-                || debris.debrisType == Debris.DebrisType.ARCHAEOLOGY
-                || debris.debrisType == Debris.DebrisType.RESOURCE
-                || debris.item != null
-            )
-                return true; //this is an item
-            else
-                return false; //this is NOT an item
-        }
-    }
 }
